04a64716c54dd1930fc982950bf11ec2967c6ebd
Leluzca20 <101336907+Leluzca20@users.noreply.github.com>
2022-05-17 17:37:59 -0500

    Update suma.cpp

--- suma.cpp
+++ suma.cpp
@@ -1,16 +1,16 @@
 #include<iostream> 
  using namespace std; 
  int main(){ 
- int x,c=0; 
- float a,n=0; 
+ int Al_x,Al_c=0; 
+ float Al_a,Al_n=0; 
  cout<<"ingrese el numero de veses que se va a sumar: "; 
- cin>>n; 
+ cin>>Al_n; 
  do{ 
  cout<<"ingrese una cantidad"<<endl; 
- cin>>a; 
- c=c+1; 
- a=a+x; 
+ cin>>Al_a; 
+ Al_c=Al_Al_c+1; 
+ Al_a=Al_a+Al_x; 
  } 
- while(c==n); 
- cout<<"la cantidad final es: "<<a; 
+ while(Al_c==Al_n); 
+ cout<<"la cantidad final es: "<<Al_a; 
  }
