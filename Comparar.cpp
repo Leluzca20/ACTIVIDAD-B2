576408cd4c17af33f9ba02a3c60d2a6d5541076d
Leluzca20 <101336907+Leluzca20@users.noreply.github.com>
2022-05-17 16:28:43 -0500

    Update Comparar.cpp

--- Comparar.cpp
+++ Comparar.cpp
@@ -6,8 +6,8 @@
 			float Al_A,Al_B;
 			cout<<"INGRESE LOS VALORES A y B";
 			cin>> Al_a,Al_B;
-		if(A==B){
-			cout<<"el valor"<<A<<"es igual a"<<B;
+		if(Al_A==Al_B){
+			cout<<"el valor"<<Al_A<<"es igual a"<<Al_B;
 		}else{
 			if(Al_A<Al_B){
 				cout<<"el valor"<<Al_A<<"es menor que"<<Al_B;
