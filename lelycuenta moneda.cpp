5065ae9782145025991b46828add6018807dd1cb
Leluzca20 <101336907+Leluzca20@users.noreply.github.com>
2022-05-17 15:13:38 -0500

    Create lelycuenta moneda.cpp

---
+++ lelycuenta moneda.cpp
@@ -0,0 +1,53 @@
+# include<iotream>
+using namespace std;
+int main()
+{
+   int n,c,ci,c2,cl,cz;
+  float
+    x,al,a,a1,a2,az;
+         c = 0;
+         c1 = 0;
+         c2 = 0; 
+          a = 0;
+  a1 = 0;
+  a2 = 0;
+  cout<<"\n\tCuenta Moneda
+    \n\n"<<endl;
+    cout<<"Cantidad de monedas a ingresar:"
+      ;cin>>n;
+          do
+  }
+                        cout<<"
+   Ingrese el valor de la moneda(0.10-50)
+         ;cin>>x ;
+            c1=c1+1 ; 
+              a1=x+a1 ;
+           if(x==0.50)
+                     {
+              c1=c1+1 ;
+              a1=x+a1 ;
+  }
+  else
+                 }
+          c2=c2+1 ;
+    a2=a2+x ;
+  }
+   while(c<n);
+cout<<"\n\tresultado\n\n"
+  <<endl;
+      cout<<"cantidad to tal de dinero contado:"
+        <<a<<endl;
+              cout<< cantidad total de  monedas de 0.10 ingresadas:"
+         <<c2<<endl; 
+              cout<< cantidad total de  monedas de 0.10:"
+           <<a2<<endl;
+                cout<< cantidad total de  monedas de 0.50 ingresadas:"
+              <<c1<<endl; 
+                 cout<< cantidad total de  monedas de 0.50 ingresadas:"<<a1<<endl;
+                   
+                   return 0;
+}
+                  
+          
+          
+          
